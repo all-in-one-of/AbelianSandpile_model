3582f4dd7e0ddacb0febf2b7cd9426d1295889ea
Deviner <31226325+Deviner75@users.noreply.github.com>
2018-08-27 22:09:34 -0700

    Updated with Pedohorse comments
    functions, loops, vectors array, neighbors and so on were removed.
    Neighbors points passed as an attribute from upstream.
    Everything has been squeezed if one loop,

--- cpp/sandDunes.h
+++ cpp/sandDunes.h
@@ -6,26 +6,18 @@
 class sandDunes : public SOP_Node
 {
 public:
-    sandDunes(OP_Network *net, const char *name, OP_Operator *op);
-    virtual ~sandDunes();
+	sandDunes(OP_Network *net, const char *name, OP_Operator *op);
+	virtual ~sandDunes();
 
-    static PRM_Template myTemplateList[];
-    static OP_Node *myConstructor(OP_Network*, const char *, OP_Operator *);
+	static PRM_Template myTemplateList[];
+	static OP_Node *myConstructor(OP_Network*, const char *, OP_Operator *);
 
 protected:
-    virtual OP_ERROR cookMySop(OP_Context &context);
+	virtual OP_ERROR cookMySop(OP_Context &context);
 private:
 	int ITERATIONS(fpreal t) { return evalInt("iters", 0, t); }
-	fpreal GETDIST(fpreal t) { return evalFloat("dist", 0, t); }
 	float GETMASS(fpreal t) { return evalFloat("mass", 0, t); }
 	float GETTHRS(fpreal t) { return evalFloat("threshold", 0, t); }
-	int GETNPT(fpreal t) { return evalInt("nptCount", 0, t); }
-
-	std::vector<float> findH();
-	std::vector<int> findNearestPoints(const GU_Detail*, fpreal, int);
-	std::vector<float> findNptH(std::vector<int>);
-	void updateData(std::vector<int>, std::vector<float>&, std::vector<float>, float, float);
-	void setH(std::vector<float>);
 };
 
 #endif
